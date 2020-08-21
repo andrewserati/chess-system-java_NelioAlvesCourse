e107901d3b92fc8163ad3490e96ce89588bf237f
SirLordX <andrewse2015@hotmail.com>
2020-08-21 00:58:33 -0300

    Little improvement in board printing

--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -5,9 +5,6 @@ public class Piece {
     protected Position position;
     private Board board;
 
-    public Piece() {
-    }
-
     public Piece(Board board) {
         this.board = board;
         position = null;

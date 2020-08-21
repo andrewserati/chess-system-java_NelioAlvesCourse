63c22899d7560008d095144062af2d381e8073a4
SirLordX <andrewse2015@hotmail.com>
2020-08-21 01:45:03 -0300

    Possible moves of a piece

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -44,6 +44,9 @@ public class ChessMatch {
         if (!board.thereIsAPiece(position)){
             throw  new ChessException("There is no piece on souce position");
         }
+        if (!board.piece(position).isThereAnyPossibleMove()){
+            throw new ChessException("There is no possible moves for the chosen piece");
+        }
     }
 
     private  void placeNewPiece(Character column, Integer row, ChessPiece piece){
--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -16,4 +16,9 @@ public class King extends ChessPiece {
         return "K";
     }
 
+    @Override
+    public boolean[][] possibleMoves() {
+        boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+        return mat;
+    }
 }
